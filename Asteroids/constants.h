0a88873e2dca8c4288d9b5e986a08a3ac0e379de
Melody Jensen <jajensen05@gmail.com>
2024-04-22 08:16:04 -0400

    fix movement

--- Asteroids/constants.h
+++ Asteroids/constants.h
@@ -4,3 +4,101 @@
 
 #define defaultWindowWidth 800
 #define defaultWindowHeight 600
+
+#define FPS 60
+#define targetFrameTime (1000/FPS)
+
+#define playerSize 8
+#define maxSpeed 5
+#define accelFactor 1
+#define decelFactor 0.01
+
+//Variable type that holds 2 dimentional information
+typedef struct {
+	float x;
+	float y;
+} vector2;
+
+//Variable type that holds shape type and vertex position data
+typedef struct {
+	int numVertecies;
+	vector2 vertecies[12];
+} shape;
+
+//Player "class"
+typedef struct {
+	vector2 pos;
+	vector2 vel;
+	vector2 input;
+	float r; //rotation
+	shape shapeType;
+	shape vertecies;
+} player;
+
+//Asteroid "class"
+typedef struct {
+	vector2 pos;
+	vector2 vel;
+	float r; //rotation
+	shape shapeType;
+	shape vertecies;
+	float size;
+	int alive;
+} asteroid;
+
+//Bullet "class"
+typedef struct {
+	vector2 pos;
+	vector2 vel;
+} boolet;
+
+//vertecies of the ship relative to ship center
+vector2 shipVertecies[4] = { 
+	{0, 3}, 
+	{-3, -3}, 
+	{0, -1.5}, 
+	{3, -3}};
+
+//vertecies of this type of asteroid relative to asteroid center
+vector2 asteroid1Vertecies[11] = {
+	{0, -1},
+	{0, -3},
+	{1, -3},
+	{3, -1},
+	{3, 1},
+	{1, 3},
+	{-1, 3},
+	{-3, 1},
+	{-1, 0},
+	{-3, -1},
+	{-1, -3}};
+
+//vertecies of this type of asteroid relative to asteroid center
+vector2 asteroid2Vertecies[12] = {
+	{1, -2},
+	{2, -3},
+	{3, -1},
+	{1, 0},
+	{3, 1},
+	{3, 2},
+	{1, 3},
+	{-2, 3},
+	{-1, 2},
+	{-3, 2},
+	{-3, -1},
+	{-2, -3}};
+
+//vertecies of this type of asteroid relative to asteroid center
+vector2 asteroid3Vertecies[12] = {
+	{-1, -2},
+	{2, -3},
+	{3, -1},
+	{2, 0},
+	{3, 1},
+	{2, 3},
+	{0, 2},
+	{-1, 3},
+	{-3, 2},
+	{-2, 0},
+	{-3, -1},
+	{-2, -3}};
--- Asteroids/main.c
+++ Asteroids/main.c
@@ -9,6 +9,28 @@ SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
 
 int isRunning = false;
+int lastFrameTime = 0;
+
+int difficulty = 0;
+
+asteroid asteroids[48]; //up to 12 total starting asteroids (level 8)
+player ship;
+
+vector2 mousePos;
+
+//Calculate vertex positions from relative position, global center position, and rotation
+shape calcVertecies(shape vertecies, float r, vector2 center, float size) {
+	shape out;
+
+	out.numVertecies = vertecies.numVertecies;
+
+	for (int i = 0; i < vertecies.numVertecies; i++) {
+		out.vertecies[i].x = playerSize * size * cos(r) * vertecies.vertecies[i].x + playerSize * size * sin(r) * vertecies.vertecies[i].y + center.x;
+		out.vertecies[i].y = playerSize * size * -sin(r) * vertecies.vertecies[i].x + playerSize * size * cos(r) * vertecies.vertecies[i].y + center.y;
+	}
+
+	return out;
+}
 
 //Setup window and it's parameters
 int initializeWindow(void) {
@@ -20,7 +42,7 @@ int initializeWindow(void) {
 
 	//Window variable setup
 	window = SDL_CreateWindow(
-		"Boids", //Window Title
+		"Asteroids", //Window Title
 		SDL_WINDOWPOS_CENTERED, //Xpos
 		SDL_WINDOWPOS_CENTERED, //Ypos
 		defaultWindowWidth, //Width
@@ -38,23 +60,33 @@ int initializeWindow(void) {
 	windowHeight = DM.h;*/
 
 	//Checks if window initialized correctly
-	if (!window) {
-		fprintf(stderr, "Error creating SDL Window.\n");
-		return false;
-	}
-
-	//Renderer variable setup 
-	renderer = SDL_CreateRenderer(window, -1, 0);
-
-	//Checks if renderer initialized correctly
-	if (!renderer) {
-		fprintf(stderr, "Error creating SDL Renderer.\n");
-		return false;
-	}
+if (!window) {
+	fprintf(stderr, "Error creating SDL Window.\n");
+	return false;
 }
 
+//Renderer variable setup 
+renderer = SDL_CreateRenderer(window, -1, 0);
+
+//Checks if renderer initialized correctly
+if (!renderer) {
+	fprintf(stderr, "Error creating SDL Renderer.\n");
+	return false;
+}
+}
+
+//Sets up the scene
 void setup() {
-	//TODO
+	//Player variable setup
+	ship.pos.x = defaultWindowWidth / 2;
+	ship.pos.y = defaultWindowHeight / 2;
+	ship.vel.x = 0;
+	ship.vel.y = 0;
+	ship.shapeType.numVertecies = 4;
+	for (int i = 0; i < 4; i++) {
+		ship.shapeType.vertecies[i] = shipVertecies[i];
+	}
+	ship.r = 0;
 }
 
 //Takes and processes inputs
@@ -76,11 +108,65 @@ int processInput() {
 			return false;
 		}
 	}
+
+	//Get mouse position
+	if (event.type == SDL_MOUSEMOTION) {
+		mousePos.x = event.motion.x;
+		mousePos.y = event.motion.y;
+	}
+
+	//If a key was pressed
+	if (event.type == SDL_KEYDOWN && event.key.repeat == 0) {
+		//Adjust the input variable
+		switch (event.key.keysym.sym) {
+		case SDLK_w: ship.input.y -= 1; break;
+		case SDLK_s: ship.input.y += 1; break;
+		case SDLK_a: ship.input.x -= 1; break;
+		case SDLK_d: ship.input.x += 1; break;
+		}
+	}
+	//If a key was released
+	else if (event.type == SDL_KEYUP && event.key.repeat == 0) {
+		//Adjust the input variable
+		switch (event.key.keysym.sym) {
+		case SDLK_w: ship.input.y += 1; break;
+		case SDLK_s: ship.input.y -= 1; break;
+		case SDLK_a: ship.input.x += 1; break;
+		case SDLK_d: ship.input.x -= 1; break;
+		}
+	}
+
+
 }
 
 //Update objects every frame
 void update() {
-	//TODO
+	//Wait an amount of time until target FPS reached
+	while (!SDL_TICKS_PASSED(SDL_GetTicks(), lastFrameTime + targetFrameTime));
+	lastFrameTime = SDL_GetTicks();
+
+	//update player velocity
+	ship.vel.x += accelFactor * ship.input.x;
+	ship.vel.y += accelFactor * ship.input.y;
+
+	if (sqrt(pow(ship.vel.x, 2) + pow(ship.vel.y, 2)) > maxSpeed) {
+		ship.vel.x = maxSpeed;
+		ship.vel.y = maxSpeed;
+	}
+
+	ship.vel.x *= 1-decelFactor;
+	ship.vel.y *= 1-decelFactor;
+
+	if (sqrt(pow(ship.vel.x, 2) + pow(ship.vel.y, 2)) < 0.5) {
+		ship.vel.x = 0;
+		ship.vel.y = 0;
+	}
+
+	//update positions
+	ship.pos.x += ship.vel.x;
+	ship.pos.y += ship.vel.y;
+
+	//printf("(%f, %f) (%f, %f)\n", ship.pos.x, ship.pos.y, ship.vel.x, ship.vel.y);
 }
 
 //Tell renderer to show objects on screen
@@ -91,33 +177,37 @@ void render() {
 	//Clear Screen
 	SDL_RenderClear(renderer);
 
-	//Loop through every boid
-	//for (int i = 0; i < numBoids; i++) {
-	//	//Calculate boid shape vertecies from position and rotation
-	//	calcVertecies(boids, i);
+	//Loop through every asteroid
+	for (int i = 0; i < 48; i++) {
+		//calculate vertecies
+		asteroids[i].vertecies = calcVertecies(asteroids[i].shapeType, asteroids[i].r * 3.1415 / 180, asteroids[i].pos, asteroids[i].size/2);
 
-	//	//set vertecies
-	//	SDL_Vertex vhead = { {boids[i].shape.head.x,boids[i].shape.head.y}, {boids[i].color.r,boids[i].color.g,boids[i].color.b,255}, {1,1} };
-	//	SDL_Vertex vlWing = { {boids[i].shape.lWing.x,boids[i].shape.lWing.y}, {boids[i].color.r,boids[i].color.g,boids[i].color.b,255}, {1,1} };
-	//	SDL_Vertex vrWing = { {boids[i].shape.rWing.x,boids[i].shape.rWing.y}, {boids[i].color.r,boids[i].color.g,boids[i].color.b,255}, {1,1} };
-	//	SDL_Vertex vtail = { {boids[i].shape.tail.x,boids[i].shape.tail.y}, {boids[i].color.r,boids[i].color.g,boids[i].color.b,255}, {1,1} };
+		//set color to white
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
-	//	//Put boid shape vertecies into array
-	//	SDL_Vertex vertices[] = {
-	//		vhead,
-	//		vlWing,
-	//		vtail,
-	//		vrWing
-	//	};
+		//draw lines
+		if (asteroids[i].alive) {
+			for (int j = 0; j < asteroids[i].vertecies.numVertecies - 1; j++) {
+				SDL_RenderDrawLine(renderer, asteroids[i].vertecies.vertecies[j].x, asteroids[i].vertecies.vertecies[j].y, asteroids[i].vertecies.vertecies[j + 1].x, asteroids[i].vertecies.vertecies[j + 1].y);
+			}
+			SDL_RenderDrawLine(renderer, asteroids[i].vertecies.vertecies[asteroids[i].vertecies.numVertecies - 1].x, asteroids[i].vertecies.vertecies[asteroids[i].vertecies.numVertecies - 1].y, asteroids[i].vertecies.vertecies[0].x, asteroids[i].vertecies.vertecies[0].y);
+		}
+	}
 
-	//	//Render boid
-	//	const int indices[] = {
-	//	   0,1,2,
-	//	   2,3,0
-	//	};
-	//	SDL_RenderGeometry(renderer, NULL, vertices, 4, indices, 6);
+	//Do same for player
+	vector2 dMousePos = {mousePos.x - ship.pos.x, mousePos.y - ship.pos.y};
+	
+	//calculate vertecies
+	ship.vertecies = calcVertecies(ship.shapeType, -atan2(dMousePos.y,dMousePos.x) + 1.571, ship.pos, 1);
 
-	//}
+	//set color to white
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+	//draw lines
+	for (int j = 0; j < ship.vertecies.numVertecies - 1; j++) {
+		SDL_RenderDrawLine(renderer, ship.vertecies.vertecies[j].x, ship.vertecies.vertecies[j].y, ship.vertecies.vertecies[j + 1].x, ship.vertecies.vertecies[j + 1].y);
+	}
+	SDL_RenderDrawLine(renderer, ship.vertecies.vertecies[ship.vertecies.numVertecies - 1].x, ship.vertecies.vertecies[ship.vertecies.numVertecies - 1].y, ship.vertecies.vertecies[0].x, ship.vertecies.vertecies[0].y);
 
 	//Swap buffer frame for current frame (Draws frame)
 	SDL_RenderPresent(renderer);
@@ -130,6 +220,7 @@ void destroyWindow() {
 	SDL_Quit();
 }
 
+//Main function
 int main(int argc, char* argv[]) {
 
 	//Set isRunning variable to if window initizalized
